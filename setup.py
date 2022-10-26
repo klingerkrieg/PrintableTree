df70c62ca77fea10d7e022b4c3c2ed1396d62333
Alan Klinger <klingerkrieg@gmail.com>
2022-10-26 15:26:47 -0300

    Publicado

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ setup(name='PrintableTree',
       author='Alan Klinger',
       author_email='klingerkrieg@gmail.com',
       description='It has an implementation of a tree in python and methods that print the tree to an image through OpenCV / É uma implementação de uma árvore em python com métodos que permitem imprimir a árvore através do OpenCV',
-      long_description=open('README.md').read(),
+      long_description='Visit the <a href="https://github.com/klingerkrieg/PrintableTree">homepage</a> for more details.',
       zip_safe=False,
       install_requires=[            # I get to this in a second
           'opencv-python',
